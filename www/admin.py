82ce5a9c568dd27358a256de1eca6e49e5149fc8
Jeremy Welch <jm-welch@users.noreply.github.com>
2017-06-09 19:13:35 -0500

    Added models to admin

--- www/admin.py
+++ www/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from.models import Clubber, Adult, Family
+from .models import Parent, Clubber, Family, Leader, Visitor,
+                    Club, Handbook, Section, Inventory,
+                    Awards, Points, Activity, Attendance, Finance, Expense
+
 # Register your models here.
 
-@admin.register(Adult)
-class AdultAdmin(admin.ModelAdmin):
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Clubber)
@@ -12,4 +15,52 @@ class ClubberAdmin(admin.ModelAdmin):
 
 @admin.register(Family)
 class FamilyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Leader)
+class LeaderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Visitor)
+class VisitorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Club)
+class ClubAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Handbook)
+class HandbookAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Section)
+class SectionAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Inventory)
+class InventoryAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Awards)
+class AwardsAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Points)
+class PointsAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Activity)
+class ActivityAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Attendance)
+class AttendanceAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Finance)
+class FinanceAdmin(models.ModelAdmin):
+    pass
+
+@admin.register(Expense)
+class ExpenseAdmin(models.ModelAdmin):
     pass
